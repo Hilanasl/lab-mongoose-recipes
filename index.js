ac463dc238677635d6dd1765b2e328f291d591ac
Hilana <hilana.sene@gmail.com>
2022-01-07 09:00:39 +0100

    done

--- index.js
+++ index.js
@@ -17,7 +17,60 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+      "title": "Brownie",
+      "level": "Easy Peasy",
+      "ingredients": [
+        "bitter chocolate",
+        "3 eggs",
+        "90gr butter",
+        "225gr suggar",
+        "125gr flour",
+        "10gr cocoa powder",
+        "75gr nuts",
+        "pince of salt",
+        "vanilla essence"
+      ],
+      "cuisine": "American",
+      "dishType": "dessert",
+      "image": "https://img.taste.com.au/1gQS58Kr/w643-h428-cfill-q90/taste/2016/11/classic-chewy-brownie-102727-1.jpeg",
+      "duration": 40,
+      "creator": "Chef Joann"
+    })
+      .then(recipe => console.log(recipe.title))
+
+    Recipe.insertMany(data)
+      .then(recipe => {
+        recipe.forEach(recipe => console.log(` --> recipe: ${recipe.title}`));
+        modifyRecipe();
+        deleteRecipe();
+      })
+      .catch(error => console.error('An error occured while creating a recipe: ', error));
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+// Update the Rigatoni recipe's duratiton
+async function modifyRecipe() {
+  try {
+    const query = { duration: 220 };
+    const found = await Recipe.findOneAndUpdate(query, { duration: 100 }, { new: true });
+    console.log('Successfuly updated the recipe: ', found.title, '--> New duration: ', found.duration);
+  } catch (err) {
+    console.error(err);
+    process.exit(0);
+  }
+}
+
+// Delete the carrot cake's recipe
+async function deleteRecipe() {
+  try {
+    const remove = await Recipe.deleteOne({ title: "Carrot Cake" });
+    console.log("Recipe successfully deleted")
+  } catch (err) {
+    console.error(err);
+    process.exit(0);
+  }
+};
+
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,9 +2,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: { type: String, required: true, unique: true },
+  level: { type: String },
+  ingredients: { enum: [] },
+  cuisine: { type: String, required: true },
+  dishType: { type: String },
+  image: { type: String },
+  duration: { type: Number, min: 0 },
+  creator: { type: String },
+  created: { type: Date, default: Date.today }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 
 module.exports = Recipe;
+
